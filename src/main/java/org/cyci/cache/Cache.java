de6dcee7c124014f2dee0738346227557b0f052d
Phil - DopeDealers <dopedealersdev@gmail.com>
2021-01-19 20:42:41 -0500

    Update

--- src/main/java/org/cyci/cache/Cache.java
+++ src/main/java/org/cyci/cache/Cache.java
@@ -15,27 +15,62 @@ import java.util.UUID;
  */
 public class Cache {
 
+    /**
+     * @Cache
+     */
     private static final Map<UUID, Player> cache;
-    public Cache() {
-    }
-
-    public static String insertUser(UUID userID, Map<UUID, Player> userInfo) throws KeyExistsException {
+    /**
+     * @Cache
+     * @return the cache
+     */
+    public static Map<UUID, Player> getCache() {return cache;}
+    /**
+     * @param userID
+     * @param userInfo
+     * @return String
+     * @throws KeyExistsException
+     * {@code Cache.insertUser(UUID, Player)}
+     */
+    public static String insertUser(UUID userID, Player userInfo) throws KeyExistsException {
         if (Cache.cache.containsKey(userID)) throw new KeyExistsException("The Key already exists in the cache.");
-        Cache.cache.put(userID, (Player) userInfo);
+        Cache.cache.put(userID, userInfo);
         return "User inserted";
     }
+    /**
+     * @param userID
+     * @return String
+     * @throws DoesNotExistException
+     * {@code Cache.removeUser(UUID)}
+     */
     public static String removeUser(UUID userID) throws DoesNotExistException {
         if (!Cache.cache.containsKey(userID)) throw new DoesNotExistException("The key does not exist in the current cache!");
         Cache.cache.remove(userID);
         return "User removed";
     }
+    /**
+     * @param userID
+     * @return boolean
+     * {@code Cache.userExists(UUID)}
+     */
     public static boolean userExists(UUID userID) {
         return Cache.cache.containsKey(userID);
     }
+
+    /**
+     * @param userID
+     * @return User
+     * @throws DoesNotExistException
+     * {@code Cache.getUser(UUID)}
+     */
     public static Cache getUser(UUID userID) throws DoesNotExistException {
         if (!Cache.cache.containsKey(userID)) throw new DoesNotExistException("The key does not exist in the current cache!");
         return (Cache) Cache.cache.get(userID);
     }
+
+    /**
+     * @Version
+     * @return
+     */
     public static String getVersion() {
         return "1.0.0";
     }
